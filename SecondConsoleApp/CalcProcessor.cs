b383ee75a34496edd4bf19c494d5bba199990d22
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-06 09:24:10 +0530

    Initial commit

---
+++ SecondConsoleApp/CalcProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondConsoleApp
+{
+    internal class CalcProcessor
+    {
+        internal double Amt { private get; set; }
+        internal double i { private get; set; }
+        internal int t { private get; set; }
+        internal int n { private get; set; }
+
+
+
+        internal double CalculateEMI()
+        {
+            double EMI = 0;
+
+            EMI = Amt * Math.Pow((1 + i/n), 2*n);
+
+            return EMI;
+        }
+
+       
+
+        
+    }
+}
---
+++ SecondConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SecondConsoleApp
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            double principal = 0;
+            double interest = 0;
+            int duration = 0;
+            int compounding = 0;
+
+
+
+            var spObject = new CalcProcessor();
+            Console.WriteLine("Enter the principle amount for EMI:");
+            principal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the rate of Interest for EMI:");
+            interest = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the duration for EMI (in years):");
+            duration = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the monthly compounding for EMI:");
+            compounding = Convert.ToInt32(Console.ReadLine());
+            
+           
+
+            spObject.Amt = principal;
+            spObject.i = interest;
+            spObject.t = duration;
+            spObject.n = compounding;
+
+            var FinalEMI = spObject.CalculateEMI();
+            FinalEMI = Math.Round(FinalEMI,3);
+            
+
+            Console.WriteLine($"The calculated EMI is : {FinalEMI}");
+            
+
+
+        }
+    }
+}
