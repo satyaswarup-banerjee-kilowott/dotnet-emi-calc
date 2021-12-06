7b46060fce7a980abb28b99952ed575abe7eeb61
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-06 15:11:55 +0530

    Initial commit

---
+++ EmiApp/EmiApp.Cmdline/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EmiApp.Cmdline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var emiRequest = new Request() { Principal = 100000, InterestRateInPercentage = 12, LoanDurationInYearCount = 2 };
+            ICalculator emiCalculator = new Calculator();
+
+            #region monthly compounding
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(emiRequest);
+            if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
+                Console.WriteLine($"Monthly EMI is {emiMonthly}");
+            else 
+                Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion 
+
+            // fill other regions below...
+
+        }
+    }
+}
+
---
+++ EmiApp/EmiApp/Calculator.cs
@@ -0,0 +1,34 @@
+﻿namespace EmiApp
+{
+    public class Calculator: ICalculator
+    {
+        public Response CalculateContinousEmi(Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response CalculateDailyEmi(Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response CalculateMonthlyEmi(Request request)
+        {
+            var response = new Response();
+            try
+            {
+                var power = request.LoanDurationInYearCount * 12;
+                var rate = request.InterestRateInPercentage / 100;
+                var accumulated = (double)request.Principal * Math.Pow(1 + rate / 12, power);
+                // convert to response object
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response = new Response() { ErrorMessage = ex.Message.ToString() };
+            }
+            finally { }
+            return response;
+        }
+    }
+}
