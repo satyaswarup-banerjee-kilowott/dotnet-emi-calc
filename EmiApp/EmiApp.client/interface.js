50849588033e54131c6ca340ce63cdbf23181e6f
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2021-12-09 16:53:30 +0530

    Initial commit

--- EmiApp/EmiApp.client/interface.js
+++ EmiApp/EmiApp.client/interface.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function() {
-    $("form").submit(function(event) {
+    $("#form1").submit(function(event) {
         var formData ={
             principal: $("#principal").val(),
             interestRateInPercentage: $("#interestRateInPercentage").val(),
@@ -26,8 +26,89 @@ $(document).ready(function() {
         }
 
         event.preventDefault();
+
+
+        
     });
 });
+
+
+
+
+
+
+$(document).ready(function() {
+    $("#form2").submit(function(event) {
+        var formData ={
+            principal: $("#principal").val(),
+            interestRateInPercentage: $("#interestRateInPercentage").val(),
+            loanDurationInYearCount: $("#loanDurationInYearCount").val(),
+        };
+    
+        $.ajax({
+            type: "POST",
+            url: "https://localhost:7281/api/emi/calculatedailyemi",
+            data: formData,
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(formData),
+            dataType: "json",
+            encode: true,
+        }).done(function(data){
+            console.log(data);
+
+            
+        });
+        (response) =>{
+            console.log(response);
+        }
+
+        event.preventDefault();
+
+
+        
+    });
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    $("#form3").submit(function(event) {
+        var formData ={
+            principal: $("#principal").val(),
+            interestRateInPercentage: $("#interestRateInPercentage").val(),
+            loanDurationInYearCount: $("#loanDurationInYearCount").val(),
+        };
+    
+        $.ajax({
+            type: "POST",
+            url: "https://localhost:7281/api/emi/calculatecontinousemi",
+            data: formData,
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(formData),
+            dataType: "json",
+            encode: true,
+        }).done(function(data){
+            console.log(data);
+
+            
+        });
+        (response) =>{
+            console.log(response);
+        }
+
+        event.preventDefault();
+
+
+        
+    });
+});
+
 
 
 
